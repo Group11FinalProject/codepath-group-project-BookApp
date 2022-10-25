da05a78a25b6d5f7b00e3a9db69e2a13892d57bc
eluzung <luzungelaine@gmail.com>
2022-10-25 02:12:05 -0400

    Added Project File

---
+++ Read and Tell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Read and Tell
+//
+//  Created by Elaine Luzung on 10/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
