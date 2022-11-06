65190d2bb6c1326027abab86e32e41dc61d2dfe2
Tasneem Hasanat <tasneemhasanat97@gmail.com>
2022-11-06 02:57:11 -0500

    Added Profile && Settings Screen

---
+++ Read and Tell/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  Read and Tell
+//
+//  Created by Tasneem Hasanat on 11/6/22.
+//
+
+import UIKit
+
+import Parse
+
+import SwiftUI
+
+class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ProfileImageView: UIImageView!
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        }
+    }
+
